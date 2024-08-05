5460608d414e78f6c7f99142d27684e7963c4ae2
Dhiren Narne <narnedhiren@gmail.com>
2024-08-05 11:35:43 -0500

    cleaning up code

--- haircutrecommender.py
+++ haircutrecommender.py
@@ -18,7 +18,9 @@ def get_hairstyle_recommendations(face_shape, desired_hair_length, gender):
     response = openai.ChatCompletion.create(
         model="gpt-4",  # or use "gpt-3.5-turbo" depending on your access
         messages=[
-            {"role": "system", "content": "You are a helpful assistant that takes in an input of a human face shape. With this information, you recommend 3 different hairstyles that fit the face shape. Along with each face shape you will provide a list of pros and cons that arise with each hairstyle."},
+            {"role": "system", "content": """You are a helpful assistant that takes in an input of a human face shape. 
+             With this information, you recommend 3 different hairstyles that fit the face shape. 
+             Along with each face shape you will provide a list of pros and cons that arise with each hairstyle."""},
             {"role": "user", "content": prompt}
         ]
     )
